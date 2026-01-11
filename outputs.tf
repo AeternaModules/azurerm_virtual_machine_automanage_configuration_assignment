e554b130a3ffbf67eeb30886461c2e8be7f812a0
Daniel Allen <dan@aeternagroup.net>
2026-01-11 22:18:36 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "virtual_machine_automanage_configuration_assignments" {
+  description = "All virtual_machine_automanage_configuration_assignment resources"
+  value       = azurerm_virtual_machine_automanage_configuration_assignment.virtual_machine_automanage_configuration_assignments
+}
+
