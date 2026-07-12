5e4501a2ac143fa4c9bb09cd26ae802a68d063e2
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:32:35 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "virtual_machine_automanage_configuration_assignments_id" {
+  description = "Map of id values across all virtual_machine_automanage_configuration_assignments, keyed the same as var.virtual_machine_automanage_configuration_assignments"
+  value       = { for k, v in azurerm_virtual_machine_automanage_configuration_assignment.virtual_machine_automanage_configuration_assignments : k => v.id }
+}
 output "virtual_machine_automanage_configuration_assignments_configuration_id" {
   description = "Map of configuration_id values across all virtual_machine_automanage_configuration_assignments, keyed the same as var.virtual_machine_automanage_configuration_assignments"
   value       = { for k, v in azurerm_virtual_machine_automanage_configuration_assignment.virtual_machine_automanage_configuration_assignments : k => v.configuration_id }
