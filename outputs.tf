462ad361bab8a867f96a68391877245c0bd0a8f8
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:22 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "virtual_machine_automanage_configuration_assignments" {
-  description = "All virtual_machine_automanage_configuration_assignment resources"
-  value       = azurerm_virtual_machine_automanage_configuration_assignment.virtual_machine_automanage_configuration_assignments
-}
 output "virtual_machine_automanage_configuration_assignments_configuration_id" {
-  description = "List of configuration_id values across all virtual_machine_automanage_configuration_assignments"
-  value       = [for k, v in azurerm_virtual_machine_automanage_configuration_assignment.virtual_machine_automanage_configuration_assignments : v.configuration_id]
+  description = "Map of configuration_id values across all virtual_machine_automanage_configuration_assignments, keyed the same as var.virtual_machine_automanage_configuration_assignments"
+  value       = { for k, v in azurerm_virtual_machine_automanage_configuration_assignment.virtual_machine_automanage_configuration_assignments : k => v.configuration_id }
 }
 output "virtual_machine_automanage_configuration_assignments_virtual_machine_id" {
-  description = "List of virtual_machine_id values across all virtual_machine_automanage_configuration_assignments"
-  value       = [for k, v in azurerm_virtual_machine_automanage_configuration_assignment.virtual_machine_automanage_configuration_assignments : v.virtual_machine_id]
+  description = "Map of virtual_machine_id values across all virtual_machine_automanage_configuration_assignments, keyed the same as var.virtual_machine_automanage_configuration_assignments"
+  value       = { for k, v in azurerm_virtual_machine_automanage_configuration_assignment.virtual_machine_automanage_configuration_assignments : k => v.virtual_machine_id }
 }
 
