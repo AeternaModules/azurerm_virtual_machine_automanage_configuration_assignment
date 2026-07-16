c5c93409384e2d5bbc97f1b26286488ae2cddb98
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:51:56 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "virtual_machine_automanage_configuration_assignments_id" {
   description = "Map of id values across all virtual_machine_automanage_configuration_assignments, keyed the same as var.virtual_machine_automanage_configuration_assignments"
-  value       = { for k, v in azurerm_virtual_machine_automanage_configuration_assignment.virtual_machine_automanage_configuration_assignments : k => v.id }
+  value       = { for k, v in azurerm_virtual_machine_automanage_configuration_assignment.virtual_machine_automanage_configuration_assignments : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "virtual_machine_automanage_configuration_assignments_configuration_id" {
   description = "Map of configuration_id values across all virtual_machine_automanage_configuration_assignments, keyed the same as var.virtual_machine_automanage_configuration_assignments"
-  value       = { for k, v in azurerm_virtual_machine_automanage_configuration_assignment.virtual_machine_automanage_configuration_assignments : k => v.configuration_id }
+  value       = { for k, v in azurerm_virtual_machine_automanage_configuration_assignment.virtual_machine_automanage_configuration_assignments : k => v.configuration_id if v.configuration_id != null && length(v.configuration_id) > 0 }
 }
 output "virtual_machine_automanage_configuration_assignments_virtual_machine_id" {
   description = "Map of virtual_machine_id values across all virtual_machine_automanage_configuration_assignments, keyed the same as var.virtual_machine_automanage_configuration_assignments"
-  value       = { for k, v in azurerm_virtual_machine_automanage_configuration_assignment.virtual_machine_automanage_configuration_assignments : k => v.virtual_machine_id }
+  value       = { for k, v in azurerm_virtual_machine_automanage_configuration_assignment.virtual_machine_automanage_configuration_assignments : k => v.virtual_machine_id if v.virtual_machine_id != null && length(v.virtual_machine_id) > 0 }
 }
 
